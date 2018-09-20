29f714fd09901970e331119922275a4e0b0c85c3
CUESTA DELGADO SEBASTIAN FRANCISCO <secuestade@uide.edu.ec>
2018-09-20 11:45:07 -0500

    clase conexion camaras

---
+++ frmMain/frmMain/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace frmMain
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.imgBox1 = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgBox1
+            // 
+            this.imgBox1.Location = new System.Drawing.Point(12, 12);
+            this.imgBox1.Name = "imgBox1";
+            this.imgBox1.Size = new System.Drawing.Size(463, 407);
+            this.imgBox1.TabIndex = 2;
+            this.imgBox1.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1302, 617);
+            this.Controls.Add(this.imgBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Artificial Vision";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imgBox1;
+    }
+}
+
---
+++ frmMain/frmMain/camara.cs
@@ -0,0 +1,60 @@
+﻿using Emgu.CV;
+using System.Windows.Forms;
+using Emgu.CV.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frmMain
+{
+    class camara
+    {
+        private VideoCapture[] capture;
+        private Mat Frame;
+        public camara()
+        {
+
+        }
+
+        public void Inicializar(int Indice,ImageBox imgBox)
+        {
+            capture[Indice] = new VideoCapture(Indice);
+            capture[Indice].ImageGrabbed += CapturarImagen(imgBox,Indice);
+            Frame = new Mat();
+
+        }
+
+        private EventHandler CapturarImagen(ImageBox imgBox, int Indice)
+        {
+            if (capture != null && capture[Indice].Ptr != IntPtr.Zero)
+            {
+                capture[Indice].Retrieve(Frame, 0);
+                imgBox.Image = Frame;
+            }
+            throw new NotImplementedException();
+        }
+
+        public void start(int Indice)
+        {
+            if (capture[Indice] != null)
+            {
+                try
+                {
+                    capture[Indice].Start();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        public void stop(int Indice)
+        {
+
+        }
+
+    }
+}
